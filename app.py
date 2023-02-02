25e9f3595cf8e87bddf80bb1df8ace41e59d0c39
Simon Eich <69088336+SimonEich@users.noreply.github.com>
2023-02-02 10:29:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,22 +6,25 @@ app = Flask(__name__)
 # Einsteigsseite, wird angezeigt mit 'http://127.0.0.1:5000
 @app.route("/", methods=["get", "post"])
 def index():
+#Create two random numbers from 1 to 6.
     w1 = random.randint(1,6)
     w2 = random.randint(1,6)
+ 
     print(w1)
     print(w2)
     
     print(request.form)
+    #If wurfel1 is pushed there gonna be showed the following text. One dice will be thrown.
     if "wurfel1" in request.form:
         print("wurfel1")
-        # Die Variable option wird in options.html verwendet
         text="Der Würfel Zeigt eine "+ str(w1)
         return render_template("index.html", text =text)
+    #If wurfel2is pushed there gonna be showed the following text. Two dice will be thrown.
     elif "wurfel2" in request.form:
         print("wurfel2")
-        # Die Variable option wird in options.html verwendet
         text="Die Würfel zeigen eine "+ str(w1)+" und eine "+ str(w2)
         return render_template("index.html", text=text)
+    #The App starts with the text "Zuerst Würfeln". This text gonna be changed with the predominate text options.
     else:
         text= "Zuerst Würfeln"
         return render_template("index.html", text =text)
